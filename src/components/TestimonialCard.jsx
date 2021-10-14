3dd49568532866f16a0df7e4f34f44c550b6256b
Diana Tineo <diana.tineo1@gmail.com>
2021-10-14 17:50:03 -0400

    testimonial card list

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -8,6 +8,7 @@ function TestimonialCard({ name, description }) {
         <div className="testimonial-card">
             <h3>{name}</h3>
             <p>{description}</p>
+            <img src={image}/>
         </div>
     )
 }
