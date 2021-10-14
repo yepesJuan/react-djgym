59438b615b45bd13bb318a737a7dfbd6e55cb925
Juan Yepes <yepes.j@hotmail.com>
2021-10-14 16:02:09 -0400

    added files

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./hero.css"
+
+function Hero() {
+    return (
+        <section className="hero">
+            <h1>DJ Workout ++</h1>
+            <h2>This is how to get started</h2>
+            <a href="https://youtu.be/zwSRF3QkG7Q" className="vid" >Workout Promo</a>
+        </section>
+    )
+}
+
+export default Hero
---
+++ src/components/TestimonialCard.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+// //import Testimonials from "./components/Testimonials.jsx"
+
+// function TestimonialCard({ name, descriptions }) {
+//    // const { brand, type, name, price } = props
+//    // const brand = props.brand
+//     return  (
+//         <div className="testimonial-card">
+//             <h3>{name}</h3>
+//             <p>{descriptions}</p>
+//         </div>
+//     )
+// }
+
+// export default TestimonialCard
