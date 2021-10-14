352cc47b9a507b97104f00fed76b1b0998805646
Diana Tineo <diana.tineo1@gmail.com>
2021-10-14 16:44:07 -0400

    added Testimonial map function

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,43 @@
 import React from "react";
 import './testimonials.css'
+import TestimonialCard from "./TestimonialCard";
+
+const testimonialList =  [
+  {
+    id: 1,
+    name: "Jake",
+    description: "I was once fat, thanks to DJ Workout ++, I am now fit",
+  
+  },
+  {
+    id: 2,
+    name: "Tom",
+    description: "DJ Workout ++ is even better than any gym or workout program",
+ 
+  },
+  {
+    id: 3,
+    name: "Maria",
+    description: "Dania and Juan are so nice. Best trainers in the world wide web!" ,
+
+  }
+];
 
 function Testimonials(){
   return(
-      <h2>Testimonials</h2>
+    <>
+     <h2>Testimonials</h2>
+     <div className="testimonial-card">
+      {testimonialList.map(person => {
+        return <TestimonialCard
+        key={person.id}
+        name={person.name}
+        description={person.description}
+        /> 
+       })
+      }
+     </div>
+     </>
   )
 }
 
